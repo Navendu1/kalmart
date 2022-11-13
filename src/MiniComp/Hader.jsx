df646863b37371ff9da08f9ccf30fd9df470470f
0d-p0 <pritam.zr@gmail.com>
2022-11-13 11:26:37 +0530

    image add

--- src/MiniComp/Hader.jsx
+++ src/MiniComp/Hader.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { Color } from '../Color'
 import { useNavigate } from "react-router-dom";
 
+import logo from '../Data/images/msmaitilogo.png'
 
-const headerStyle = { backgroundColor:Color.TrueBlue, padding: 10, color: "white", display: 'flex', justifyContent: "space-around",marginBottom:'10px' }
+const headerStyle = { backgroundColor:Color.TrueBlue, color: "white", display: 'flex', justifyContent: "space-around", marginBottom:'10px' }
 
 function Hader() {
     let navigate = useNavigate();
@@ -13,7 +14,7 @@ function Hader() {
             <div style={headerStyle}>
                 <div className='click' onClick={()=>navigate('/')}>
                  {/* onclick  goto home page*/}
-                    <img src="" alt="" />
+                    <img src={logo} alt="" style={{height:"30px",width:'30px'}} />
                 </div>
                 <div className='click'>
                     {/* onclick  goto login page*/}
